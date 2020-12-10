0363c990bbca046d3f21c3b30f6cd21c1a713d31
ted <tedmarov@gmail.com>
2020-12-10 15:26:01 -0600

    User can logout

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom"
 import "./NavBar.css"
 
 export const NavBar = (props) => {
+
+    const handleLogout = (e) => {
+        e.preventDefault()
+        localStorage.clear()
+        props.history.push("/login")
+    }
+
+
     return (
         <ul className="navbar">
             <li className="navbar__item active">
@@ -11,8 +19,8 @@ export const NavBar = (props) => {
             <li className="navbar__item">
                 <Link className="navbar__link" to="/events">Events</Link>
             </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/logout">Logout</Link>
+            <li className="navbar__item" >
+                <Link className="navbar__link" onClick={handleLogout}>Logout</Link>
             </li>
         </ul>
     )
